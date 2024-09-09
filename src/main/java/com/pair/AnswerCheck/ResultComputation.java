af50b835de65c3935408ec9cc5b17ba7c48d2c57
star <13782156+star1908@user.noreply.gitee.com>
2024-09-09 19:34:00 +0800

    计算注释

--- src/main/java/com/pair/AnswerCheck/ResultComputation.java
+++ src/main/java/com/pair/AnswerCheck/ResultComputation.java
@@ -20,51 +20,56 @@ public class ResultComputation {
                 String[] strArr = {};
                 String[] strArr1 = {};
 
+                // 分离分子与分母
                 strArr = arr[i - 1].split("/");
                 arr[i - 1] = strArr[0];
                 strArr1 = arr[i + 1].split("/");
                 arr[i + 1] = strArr1[0];
 
                 switch (arr[i]) {
-                    case ADD:
+                    case ADD: // 调用加法，如遇分数则乘数乘以对方分母再加上分子
                         if(strArr.length != 1) arr[i + 1] = String.valueOf(Integer.parseInt(arr[i + 1]) * Integer.parseInt(strArr[1]));
                         if(strArr1.length != 1) arr[i - 1] = String.valueOf(Integer.parseInt(arr[i - 1]) * Integer.parseInt(strArr1[1]));
                         arr[i + 1] = String.valueOf(Integer.parseInt(arr[i - 1]) + Integer.parseInt(arr[i + 1]));
                         break;
-                    case SUB:
+                    case SUB: // 调用减法，如遇分数则乘数乘以对方分母再减去分子
                         if(strArr.length != 1) arr[i + 1] = String.valueOf(Integer.parseInt(arr[i + 1]) * Integer.parseInt(strArr[1]));
                         if(strArr1.length != 1) arr[i - 1] = String.valueOf(Integer.parseInt(arr[i - 1]) * Integer.parseInt(strArr1[1]));
                         arr[i + 1] = String.valueOf(Integer.parseInt(arr[i - 1]) - Integer.parseInt(arr[i + 1]));
                         break;
-                    case MUL:
+                    case MUL: // 调用乘法，分子互乘，分母互乘
                         if(strArr.length != 1 && strArr1.length != 1) {
                             strArr[1] = String.valueOf(Integer.parseInt(strArr[1]) * Integer.parseInt(strArr1[1]));
                             strArr1 = new String[]{strArr1[0]};
                         }
                         arr[i + 1] = String.valueOf(Integer.parseInt(strArr[0]) * Integer.parseInt(strArr1[0]));
                         break;
-                    case DIV:
+                    case DIV: // 调用除法，结果为假分数，例如6/4/3，后续化简
                         if(strArr1.length != 1) {
+                            // 除以分数相当于乘其倒数，故翻转为倒数
                             strArr1 = new String[]{strArr1[1], strArr1[0]};
 
+                            // 调用乘法
                             if(strArr.length != 1) {
                                 strArr[1] = String.valueOf(Integer.parseInt(strArr[1]) * Integer.parseInt(strArr1[1]));
                                 strArr1 = new String[]{strArr1[0]};
                             }
                             arr[i + 1] = String.valueOf(Integer.parseInt(strArr[0]) * Integer.parseInt(strArr1[0]));
                         }
-                        else arr[i + 1] = arr[i - 1] + "/" + arr[i + 1];
+                        else arr[i + 1] = arr[i - 1] + "/" + arr[i + 1]; // 除法以分数形式，后续化简
                         break;
                 }
 
+                // 加上分母
                 if(strArr.length != 1) arr[i + 1] = arr[i + 1] + "/" + strArr[1];
                 if(strArr1.length != 1) arr[i + 1] = arr[i + 1] + "/" + strArr1[1];
                 arr[i] = "";
                 arr[i - 1] = "";
 
-                System.out.println(Arrays.toString(arr));
             }
+
         }
+        System.out.println("结果为：" + arr[arr.length - 2]);
 
         return result;
     }
